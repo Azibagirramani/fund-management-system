0be43fbfd55c6968eadef2841ea53cee66873eee
Azibagirramani <kelvinmansi2@gmail.com>
2022-01-08 20:08:59 +0100

    add employee

--- src/module/Organisation/org.routes.js
+++ src/module/Organisation/org.routes.js
@@ -1,19 +1,18 @@
 const routes = [
-    {
-        path: '/organisation',
-        component: ()=> import("./index.vue"),
-        children: [
-            {
-                path: 'Dashboard',
-                component: () => import("../../views/dashboard.vue"),
-            },
-            {
-                path: 'setup',
-                component: () => import("../Setup/index.vue"),
-            }
-        ]
-    }
-]
+  {
+    path: "/organisation",
+    component: () => import("./index.vue"),
+    children: [
+      {
+        path: "Dashboard",
+        component: () => import("../../views/dashboard.vue"),
+      },
+      {
+        path: "setup",
+        component: () => import("../Setup/index.vue"),
+      },
+    ],
+  },
+];
 
-
-export default routes
+export default routes;
--- src/module/Setup/index.vue
+++ src/module/Setup/index.vue
@@ -5,7 +5,7 @@
       <div class="card shadow border-0 w-75">
         <div class="card-body">
           <form-wizard>
-            <tab-content title="Organization profile" :selected="true">
+            <tab-content title="Organization profile">
               <form @submit.prevent="onSubmit(form)">
                 <div class="mt-4">
                   <!-- Organization logo -->
@@ -185,7 +185,11 @@
               <div>
                 <p class="text-muted">Add Departments</p>
                 <div class="row row-cols-3 g-0 gy-2 my-3">
-                  <div class="col" v-for="(dep, index) in departments" :key="index">
+                  <div
+                    class="col"
+                    v-for="(dep, index) in departments"
+                    :key="index"
+                  >
                     <span class="badge rounded-pill bg-light text-dark">
                       {{ dep }}
                       <i
@@ -209,34 +213,157 @@
                 </div>
               </div>
             </tab-content>
-            <tab-content title="Project managers">
-              <div>
-                <p class="text-muted">Add Project Manager</p>
-                <div class="row row-cols-3 g-0 gy-2 my-3">
-                  <div class="col" v-for="(pm, index) in projectManagers" :key="index">
-                    <span class="badge rounded-pill bg-light text-dark">
-                      {{ pm }}
-                      <i
-                        class="fa fa-times"
-                        aria-hidden="true"
-                        @click="removePm(index)"
-                      ></i
-                    ></span>
+            <tab-content title="Project managers" :selected="true">
+              <form>
+                <div class="row mt-4">
+                  <p class="text-muted">Add Employees</p>
+                  <div class="row">
+                    <div class="col">
+                      <div class="form-floating mb-3">
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="companyName"
+                          placeholder="Enter company name"
+                          v-model="form.firstName"
+                          required
+                        />
+                        <label for="floatingInput">First Name</label>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="form-floating mb-3">
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="companyWebsite"
+                          placeholder="Enter company Description"
+                          v-model="form.lastName"
+                          required
+                        />
+                        <label for="companyWebsite">Last Name</label>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row mt-3">
+                    <div class="col">
+                      <div class="form-floating mb-3">
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="companyWebsite"
+                          placeholder="User Name"
+                          v-model="form.userName"
+                          required
+                        />
+                        <label for="companyWebsite">User Name</label>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="form-floating mb-3">
+                        <input
+                          type="email"
+                          class="form-control"
+                          id="companyWebsite"
+                          placeholder="Email"
+                          v-model="form.email"
+                          required
+                        />
+                        <label for="companyWebsite">Email</label>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <div class="form-floating mb-3">
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="companyWebsite"
+                          placeholder="Enter company Description"
+                          v-model="form.phone"
+                          required
+                        />
+                        <label for="companyWebsite">Phone</label>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="form-floating mb-3">
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="companyWebsite"
+                          placeholder="Enter department ID"
+                          v-model="form.departmentId"
+                          required
+                        />
+                        <label for="companyWebsite">Department ID</label>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <div class="form-floating mb-3">
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="companyWebsite"
+                          placeholder="Enter company Description"
+                          v-model="form.designation"
+                          required
+                        />
+                        <label for="companyWebsite">Designation</label>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="form-floating mb-3">
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="companyWebsite"
+                          placeholder="Enter Organisation ID"
+                          v-model="form.organisationId"
+                          required
+                        />
+                        <label for="companyWebsite">Organisation ID</label>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <div class="form-floating mb-3">
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="companyWebsite"
+                          placeholder="Enter company Description"
+                          v-model="form.password"
+                          required
+                        />
+                        <label for="companyWebsite">Password</label>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="form-floating mb-3">
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="companyWebsite"
+                          placeholder="Enter Organisation ID"
+                          v-model="form.locationId"
+                          required
+                        />
+                        <label for="companyWebsite">Location ID</label>
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <div class="input-group mb-3 mt-4">
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Enter department name"
-                    v-model="projectManagersName"
-                    required
-                  />
-                  <span class="input-group-text" @click="addPm"
-                    ><i class="fa fa-plus" aria-hidden="true"></i
-                  ></span>
+
+                <div class="d-flex gap-4">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                  <button type="reset" class="btn btn-danger">Clear</button>
                 </div>
-              </div>
+              </form>
             </tab-content>
             <tab-content title="Finish">
               <div class="text-center">
